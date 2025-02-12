f412fb28e769e1d2fbd250fef7b7691cb2c0767b
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2025-02-12 17:44:47 -0400

    references added to script.js and hover animations to color buttons in styles.css

---
+++ script.js
@@ -0,0 +1,10 @@
+// counter reference
+const counterNumber = document.getElementById("count");
+
+//reference to each color button and color buttons class
+const Byellow = document.getElementById("yellow");
+const Bred = document.getElementById("red");
+const Bblue = document.getElementById("blue");
+const Bgreen = document.getElementById("green");
+
+const Bcolors = document.querySelectorAll("simon");
